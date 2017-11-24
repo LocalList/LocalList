89fca52c23106d1c73b6ee6a310f86de59a26315
straboy <straboy.lin@gmail.com>
2017-11-24 15:10:19 -0800

    clean code with consistent spaces

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
 const Home = (props) => {
-    return (
-        <div>
-            <h1>Local List</h1>
-            <div>
-                <h3>Have a job you would like done?</h3>
-                <h5>Enter a location to post a job in your community:</h5>
-                <input onChange={props.posterSearch} type="text" />
-                <button>Go!</button>
-            </div>
-            <div>
-                <h3>Are you a handyman?</h3>
-                <h5>Enter a location to search jobs near you:</h5>
-                <input onChange={props.handymanSearch} type="text" />
-                <button>Go!</button>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Local List</h1>
+      <div>
+        <h3>Have a job you would like done?</h3>
+        <h5>Enter a location to post a job in your community:</h5>
+        <input onChange={props.posterSearch} type="text" />
+        <button>Go!</button>
+      </div>
+      <div>
+        <h3>Are you a handyman?</h3>
+        <h5>Enter a location to search jobs near you:</h5>
+        <input onChange={props.handymanSearch} type="text" />
+        <button>Go!</button>
+      </div>
+    </div>
+  )
 }
 
 module.exports = Home;
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,16 +2,16 @@ const React = require('react');
 const NavLink = require('react-router-dom').NavLink;
 
 const Nav = () => {
-    return (
-        <div className="nav">
-            <NavLink activeClassName="active" to="/login"><button>Log in</button></NavLink>
-            <NavLink activeClassName="active" to="/logout"><button>Log out</button></NavLink>
-            <NavLink activeClassName="active" to="/signup"><button>Sign up</button></NavLink>
-            <NavLink activeClassName="active" to="/protected"><button>Members Only!</button></NavLink>
-            <NavLink activeClassName="active" to="/job/creation"><button>Create Job</button></NavLink>
-            <NavLink activeClassName="active" to="/profile"><button>Profile</button></NavLink>
-        </div>
-    )
+  return (
+    <div className="nav">
+      <NavLink activeClassName="active" to="/login"><button>Log in</button></NavLink>
+      <NavLink activeClassName="active" to="/logout"><button>Log out</button></NavLink>
+      <NavLink activeClassName="active" to="/signup"><button>Sign up</button></NavLink>
+      <NavLink activeClassName="active" to="/protected"><button>Members Only!</button></NavLink>
+      <NavLink activeClassName="active" to="/job/creation"><button>Create Job</button></NavLink>
+      <NavLink activeClassName="active" to="/profile"><button>Profile</button></NavLink>
+    </div>
+  )
 }
 
 module.exports = Nav;
